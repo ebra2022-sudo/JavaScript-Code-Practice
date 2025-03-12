0784a20b67a331502a7f8825568872e9f3160500
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-03-12 17:04:14 +0300

    comment in javascript

---
+++ comment_in_javascript.js
@@ -0,0 +1,19 @@
+// This is single line comment in javascript
+console.log("Hello Muhammed");
+
+/*
+This is multiline commnet in javascript
+*/
+
+console.log("Hello javascript");
+
+/**
+ * This is documentation comment in javascript 
+ * @param {number} a - first number
+ * @param {number} b - second number
+ * @returns {number} - the sum of @argument {a} and @argument {b}
+ */
+
+function sum(a,b) {
+    return a +b;
+}
